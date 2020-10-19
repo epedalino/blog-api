bcf7a4ad5137edb8f68670f3ce24da288d57bb64
epedalino <epedalino@gmail.com>
2020-10-19 14:54:37 -0400

    Add a navigation guard. User gets redirected to the main Posts page if user is not authenticated

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch} from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
     render () {
         return (
             <div className="Blog">
@@ -30,8 +33,11 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="posts" />
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
         );
